1a66effef5d0bcaf86bfa69e164be22c7859dcc9
Vrushabh Shamkuwar <vrushabh@hudsondata.com>
2022-05-15 12:28:21 +0530

    welcome and logout

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -8,12 +8,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
- 
+ name!: any;
 constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.name=localStorage.getItem('name')
   }
+
   btnClick=  () => {
     this.router.navigateByUrl('/');
+    localStorage.removeItem('name');
    };
 }
